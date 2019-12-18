7f9c31b3eb6ef4d4fbd0011109a479d7b4c1bea8
Vince Burka <vburka@chemaxon.com>
2019-12-18 15:44:06 +0100

    bat installer added for ci installation of the necessary libraries on jenknis

---
+++ ci/InstallLibraries.bat
@@ -0,0 +1,7 @@
+python -m pip install --upgrade pip
+pip install requests
+pip install pytest
+pip install jsonpath
+pip install selenium
+pip install unittest-data-provider
+REM pip install pytest-allure-adaptor
